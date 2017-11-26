1538a6e699d9d972ba86cfd545feac68dc5d6a82
Shubhransu <shubhransunandi@gmail.com>
2017-11-27 00:45:02 +0530

    nice committing

---
+++ HelloWorldSimple/src/com/shubhu/check/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.shubhu.check;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My dad is the best dad......");
+
+	}
+
+}
